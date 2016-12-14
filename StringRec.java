848157dcf110191b371095e5fa8793d8a2918bd0
Jorge <lopez.jorge1@outlook.com>
2016-12-14 12:34:03 -0500

    Adjust decompress method to account for digit before letter

--- StringRec.java
+++ StringRec.java
@@ -1,6 +1,10 @@
 public class StringRec {
 	public static void main(String[] args) {
-		System.out.println(decompress("5t5t"));
+		System.out.println(decompress("q9w5e2rt5y4qw2Er3T"));
+	}
+
+	public static String decompress(String compressedText) {
+		return decompress(compressedText,"");
 	}
 
 	private static String decompress(String compressedText, String currentString) {
@@ -12,26 +16,17 @@ public class StringRec {
 
 		if (Character.isDigit(compressedText.charAt(0))) {
 			if (compressedText.length() > 1) {
-				if (compressedText.charAt(0) == '1') {
-					return decompress(compressedText.substring(1,compressedText.length()),newString);
-				} else { 
+				if (compressedText.charAt(0) != '1') {
 					char digit = compressedText.charAt(0);
 					digit--;
 
-					newString += newString.charAt(newString.length()-1); 
+					newString += compressedText.charAt(1);
 					return decompress(Character.toString(digit) + compressedText.substring(1,compressedText.length()),newString);
+				} else {
+					newString += compressedText.charAt(1);
+					return decompress(compressedText.substring(2,compressedText.length()),newString);
 				}
-			} else if (compressedText.length() == 1) {
-				if (compressedText.charAt(0) == '1') {
-					return newString;
-				} else { 
-					char digit = compressedText.charAt(0);
-					digit--;
-					
-					newString += newString.charAt(newString.length()-1); 
-					return decompress(Character.toString(digit),newString);
-				}
-			}
+			} 
 		} else {
 			if (compressedText.length() > 1) {
 				newString += compressedText.charAt(0);
@@ -41,9 +36,5 @@ public class StringRec {
 			}
 		}
 		return newString;
-	}
-
-	public static String decompress(String compressedText) {
-		return decompress(compressedText,"");
 	}
 }
